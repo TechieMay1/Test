9c8301d3de74099e5df4aac69272798ac67a47bf
TechieMay1 <eprange@oakland.edu>
2023-01-31 16:10:55 -0500

    First Commit

---
+++ src/p2/Ma.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class Ma {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ma!");
+    }
+    
+}
---
+++ src/package1/Demo.java
@@ -0,0 +1,11 @@
+package package1;
+
+class T {
+
+}
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
